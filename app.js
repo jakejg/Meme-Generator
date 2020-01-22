1a00d8e5f1640234b7d000d429fc5d94599bcb7e
Jake <jakefrisbee@gmail.com>
2020-01-21 18:47:18 -0700

    added background color

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
-const form=document.querySelector('form')
-const link=document.querySelector('#link')
-const topText=document.querySelector('#texttop')
-const bottomText=document.querySelector('#textbottom')
-const bigContainer=document.querySelector('.box')
-const button=document.querySelector('button')
-let deleteToggle=true;
+const form = document.querySelector('form')
+const link = document.querySelector('#link')
+const topText = document.querySelector('#texttop')
+const bottomText = document.querySelector('#textbottom')
+const bigContainer = document.querySelector('.box')
+const button = document.querySelector('button')
+let deleteToggle = true;
 
 
-form.addEventListener('submit', e=>{
+form.addEventListener('submit', e =>{
     e.preventDefault()
     //create image container
-let container=document.createElement('div')
+let container = document.createElement('div')
     bigContainer.append(container)
-    container.className="image-container"
+    container.className = "image-container"
     //create image 
-let memeImg=document.createElement('img')
+let memeImg = document.createElement('img')
     container.append(memeImg)
     memeImg.setAttribute('src', link.value)
 //create div with top text
-let textTop=document.createElement('div')
+let textTop = document.createElement('div')
     container.append(textTop)
-    textTop.innerText=topText.value
+    textTop.innerText = topText.value
     textTop.className="top"
 //create div with bottom text
-let textBottom=document.createElement('div')
+let textBottom = document.createElement('div')
     container.append(textBottom)
-    textBottom.innerText=bottomText.value
-    textBottom.className="bottom"
+    textBottom.innerText = bottomText.value
+    textBottom.className = "bottom"
     form.reset()
 
 })
 
 
 
-button.addEventListener('click', (e)=>{
+button.addEventListener('click', e =>{
     let containers=document.querySelectorAll('.image-container')
     // Toggle delete on
     if(deleteToggle){
         for(image of containers){
-            image.innerHTML+=`<i class="fas fa-trash"></i>`
+            image.innerHTML += `<i class="fas fa-trash"></i>`
              //remove a meme
-            image.addEventListener('click',e=>{
-                if (e.target.tagName==="I"){
+            image.addEventListener('click', e =>{
+                if (e.target.tagName === "I"){
                     e.target.parentElement.remove()}
                                               })
                                  }
