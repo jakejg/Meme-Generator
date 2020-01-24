c9d6d60e3a0ebac08d96156cff7fd74a4a91664f
Jake <jakefrisbee@gmail.com>
2020-01-24 13:16:54 -0700

    saving current version

--- app.js
+++ app.js
@@ -8,33 +8,34 @@ let deleteToggle = true;
 
 
 form.addEventListener('submit', e =>{
-    e.preventDefault()
-    //create image container
-let container = document.createElement('div')
-    bigContainer.append(container)
-    container.className = "image-container"
+        e.preventDefault()
+    //create image container and trash icon
+    let container = document.createElement('div')
+        bigContainer.append(container)
+        container.className = "image-container"
+
     //create image 
-let memeImg = document.createElement('img')
-    container.append(memeImg)
-    memeImg.setAttribute('src', link.value)
+    let memeImg = document.createElement('img')
+        container.append(memeImg)
+        memeImg.setAttribute('src', link.value)
 //create div with top text
-let textTop = document.createElement('div')
-    container.append(textTop)
-    textTop.innerText = topText.value
-    textTop.className="top"
+    let textTop = document.createElement('div')
+        container.append(textTop)
+        textTop.innerText = topText.value
+        textTop.className="top"
 //create div with bottom text
-let textBottom = document.createElement('div')
-    container.append(textBottom)
-    textBottom.innerText = bottomText.value
-    textBottom.className = "bottom"
-    form.reset()
+    let textBottom = document.createElement('div')
+        container.append(textBottom)
+        textBottom.innerText = bottomText.value
+        textBottom.className = "bottom"
+        form.reset()
 
 })
 
 
 
 button.addEventListener('click', e =>{
-    let containers=document.querySelectorAll('.image-container')
+    let containers = document.querySelectorAll('.image-container')
     // Toggle delete on
     if(deleteToggle){
         for(image of containers){
@@ -51,9 +52,9 @@ button.addEventListener('click', e =>{
 
     else{
         for(image of containers){
-                if (image.lastElementChild.tagName==="I"){
+                if (image.lastElementChild.tagName === "I"){
                      image.lastElementChild.remove()}}
-                   deleteToggle=true
+                   deleteToggle = true
          }
                                          })
     
